2913e7bcae4c69c7307d3f1a7e3482f1afb357b6
Ondřej Langr <hi@ondrejlangr.cz>
2022-12-26 23:11:01 +0100

    Prepare development

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('partials.content-page')
-    <div id="vue">Vue</div>
+    <div id="vue"> vue </div>
     <div id="react">React</div>
   @endwhile
 @endsection
