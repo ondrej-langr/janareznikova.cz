f6451df8c61bafc6fd4e7ff4d1b8c85427e2e0c7
Ondřej Langr <hi@ondrejlangr.cz>
2023-02-19 23:59:59 +0100

    Update listener

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,11 +2,11 @@
   class="site-header duration-300"
   x-data="{ isActive: false, disapearAfter: 0 }"
   x-init="$nextTick(() => {
-    const size = Array.from(document.body.querySelectorAll('.page-header'))[0]?.offsetHeight ?? 0;
-      disapearAfter = size ? size - (document.body.querySelector('header.site-header')?.offsetHeight * 2): size;
+      const size = Array.from(document.body.querySelectorAll('.page-header'))[0]?.offsetHeight ?? 0;
+      disapearAfter = size ? size - (document.body.querySelector('header.site-header')?.offsetHeight * 2) : size;
    })"
   :class=" isActive ? 'opacity-0 -translate-y-full' : 'opacity-100' "
-  @scroll.window.debounce.150ms="disapearAfter ? (isActive=window.scrollY >= disapearAfter) : null"
+  @scroll.window.debounce.50ms="disapearAfter ? (isActive=window.scrollY >= disapearAfter) : null"
 >
   <div class="max-w-7xl px-5 mx-auto flex justify-between items-center md:flex-row-reverse" x-data="{ isMobileOpen: false }" @resize.window="isMobileOpen = false" @scroll.window.debounce.150ms="isMobileOpen = false">
     <nav x-bind:data-open="isMobileOpen">
